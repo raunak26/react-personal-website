c164780401e4564934f783e493acf8a1c622794b
Raunak Anand <rsanand@ucdavis.edu>
2021-08-04 23:10:36 -0700

    trying to add meta tags again

--- src/pages/work/work.js
+++ src/pages/work/work.js
@@ -2,6 +2,7 @@ import './work.css';
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import MetaTags from 'react-meta-tags';
 
 import Work_body from '../../components/work/work_body';
 import Footer from '../../components/footer/footer';
@@ -11,49 +12,12 @@ const Work = () =>{
 	return(
 		<React.Fragment>
 			<div className="work">
-				<Helmet>
-        			<title>Raunak Anand</title>
-        			<meta
- 						name="description"
-          				content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!"
-        			/>
-
-        			{/* <!-- Google / Search Engine Tags --> */}
-			        <meta itemprop="name" content="Raunak Anand" />
-			        <meta
-			          itemprop="description"
-			          content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!"
-			        />
-			        <meta
-			          itemprop="image"
-			          content="https://www.raunakanand.com/images/me.png"
-			        />
-
-			        {/* <!-- Facebook Meta Tags --> */}
-			        <meta property="og:url" content="https://www.raunakanand.com" />
-			        <meta property="og:type" content="website" />
-			        <meta property="og:title" content="Raunak Anand" />
-			        <meta
-			          property="og:description"
-			          content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!"
-			        />
-			        <meta
-			          property="og:image"
-			          content="https://www.raunakanand.com/images/me.png"
-			        />
-
-			        {/* <!-- Twitter Meta Tags --> */}
-			        <meta name="twitter:card" content="summary_large_image" />
-			        <meta name="twitter:title" content="Raunak Anand" />
-			        <meta
-			          name="twitter:description"
-			          content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!"
-			        />
-			        <meta
-			          name="twitter:image"
-			          content="https://www.raunakanand.com/images/me.png"
-			        />
-    			</Helmet>
+				<MetaTags>
+		            <title>Raunak Anand</title>
+		            <meta name="description" content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!" />
+		            <meta property="og:title" content="Raunak Anand" />
+		            <meta property="og:image" content="images/me.png" />
+		         </MetaTags>
 				<Navbar />
 				<div class="work_content">
 					<Work_body />
