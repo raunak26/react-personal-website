7dede3808d15a66472dbde0066450a18bc0ab402
Raunak Anand <rsanand@ucdavis.edu>
2021-08-08 16:16:41 -0700

    Added fade-in effect

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import MetaTags from 'react-meta-tags';
+import FadeIn from 'react-fade-in';
 
 import Index_body from '../../components/index/index_body';
 import Footer from '../../components/footer/footer';
@@ -11,21 +12,23 @@ import Navbar from '../../components/navbar/navbar';
 const Index = () =>{
 	return(
 		<React.Fragment>
-			<div className="index">
-				<MetaTags>
-		            <title>Raunak Anand</title>
-		            <meta name="description" content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!" />
-		            <meta property="og:title" content="Raunak Anand" />
-		            <meta property="og:image" content="images/me.png" />
-		         </MetaTags>
-				<Navbar />
-				<div class="index_content">
-					<Index_body />
+			<FadeIn transitionDuration={(1000)}>
+				<div className="index">
+					<MetaTags>
+			            <title>Raunak Anand</title>
+			            <meta name="description" content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!" />
+			            <meta property="og:title" content="Raunak Anand" />
+			            <meta property="og:image" content="images/me.png" />
+			         </MetaTags>
+					<Navbar />
+					<div class="index_content">
+						<Index_body />
+					</div>
+					<div class="index_footer">
+						<Footer />
+					</div>
 				</div>
-				<div class="index_footer">
-					<Footer />
-				</div>
-			</div>
+			</FadeIn>
 		</React.Fragment>
 	);
 }
--- src/pages/work/work.js
+++ src/pages/work/work.js
@@ -3,6 +3,7 @@ import './work.css';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import MetaTags from 'react-meta-tags';
+import FadeIn from 'react-fade-in';
 
 import Work_body from '../../components/work/work_body';
 import Footer from '../../components/footer/footer';
@@ -11,21 +12,23 @@ import Navbar from '../../components/navbar/navbar';
 const Work = () =>{
 	return(
 		<React.Fragment>
-			<div className="work">
-				<MetaTags>
-		            <title>Raunak Anand</title>
-		            <meta name="description" content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!" />
-		            <meta property="og:title" content="Raunak Anand" />
-		            <meta property="og:image" content="images/me.png" />
-		         </MetaTags>
-				<Navbar />
-				<div class="work_content">
-					<Work_body />
+			<FadeIn transitionDuration={(1000)}>
+				<div className="work">
+					<MetaTags>
+			            <title>Raunak Anand</title>
+			            <meta name="description" content="Hi👋 My name is Raunak Anand! Welcome to my website! I’m a rising senior studying Computer Science and Statistics at UC Davis. I’m interested in AR/VR and ML!" />
+			            <meta property="og:title" content="Raunak Anand" />
+			            <meta property="og:image" content="images/me.png" />
+			         </MetaTags>
+					<Navbar />
+					<div class="work_content">
+						<Work_body />
+					</div>
+					<div class="work_footer">
+						<Footer />
+					</div>
 				</div>
-				<div class="work_footer">
-					<Footer />
-				</div>
-			</div>
+			</FadeIn>
 		</React.Fragment>
 	);
 }
